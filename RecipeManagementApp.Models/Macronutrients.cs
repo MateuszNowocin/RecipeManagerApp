157f0b976dfa9df1b5546532b11f5c6bfbea8c5e
Mateusz Nowocin <matimatima@gmail.com>
2024-08-17 17:39:27 +0200

    Add Macronutrients class

---
+++ RecipeManagementApp.Models/Macronutrients.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeManagementApp.Models
+{
+    public class Macronutrients
+    {
+        public double Protein { get; set; }
+        public double Carbohydrates { get; set; }
+        public double Sugar { get; set; }
+        public double Fat { get; set; }
+        public double SaturatedFat { get; set; }
+        public double Fiber { get; set; }
+
+        public Macronutrients(double protein, double carbs, double sugar, double fat, double saturatedFat, double fiber)
+        {
+            Protein = protein;
+            Carbohydrates = carbs;
+            Sugar = sugar;
+            Fat = fat;
+            SaturatedFat = saturatedFat;
+            Fiber = fiber;
+        }
+
+        public double TotalCarbohydrates => Carbohydrates + Sugar;
+        public double TotalFat => Fat + SaturatedFat;
+    }
+}
